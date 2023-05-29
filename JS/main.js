9c99c272ddf23745be94d59fa362a79f415a5aaf
HaxProf <botnar.van@gmail.com>
2023-05-29 20:51:43 +0300

    First commit

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+$(".header__burger").on("click", function (e) {
+    e.preventDefault();
+    $(".header__nav").toggleClass("header__nav--open");
+	$(".overlay").toggleClass("overlay--active");
+  });
