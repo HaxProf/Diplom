17db9403e9e2de548ef55bdff1a3f7d8335e2a9f
HaxProf <botnar.van@gmail.com>
2023-06-05 16:34:12 +0300

    Commit9

--- JS/main.js
+++ JS/main.js
@@ -7,3 +7,14 @@ $(".burger, .overlay").on("click", function (e) {
   e.preventDefault();
   $(".burger").toggleClass("burger__open");
 });
+$(function () {
+  var mixer = mixitup(".target");
+});
+
+$(document).ready(function () {
+  $(".services__tabs-link").click(function (e) {
+    e.preventDefault();
+    $(".services__tabs-body").removeClass("services__tabs-body--active");
+    $($(this).attr("href")).addClass("services__tabs-body--active");
+  });
+});
